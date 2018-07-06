8bad16126717fd2416078aa6d200de91a7141dd2
Tige Phillips <tigelane@mac.com>
2018-07-06 14:10:12 -0700

    Names changes and additions to installers

---
+++ debian_install _docker.sh
@@ -0,0 +1,13 @@
+# wget -O debian_install_docker.sh https://raw.githubusercontent.com/tigelane/system_utilities/master/install%20_docker_ubuntu.sh
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get -f install -y
+# folowing for debian
+apt install docker-ce=17.03.2~ce-0~ubuntu-xenial
+# Following for pure ubuntu
+#sudo apt-get install -y docker-ce
+#sudo systemctl status docker
+sudo usermod -aG docker ${USER}
