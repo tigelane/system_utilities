78660745543743a41d1ee1eba5f274724f5d030c
Tige Phillips <tigelane@mac.com>
2018-07-06 14:20:52 -0700

    the times they are a changin

--- debian_install_docker.sh
+++ debian_install_docker.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
@@ -12,5 +13,5 @@ sudo usermod -aG docker ${USER}
 
 # Download and run
 exit
-wget -O debian_install_docker.sh https://raw.githubusercontent.com/tigelane/system_utilities/master/install%20_docker_ubuntu.sh
-chmod +x *_install_docker.#!/bin/sh
+wget -O debian_install_docker.sh https://raw.githubusercontent.com/tigelane/system_utilities/master/install_docker_ubuntu.sh && chmod +x debian_install_docker.sh
+chmod +x *_install_docker.
