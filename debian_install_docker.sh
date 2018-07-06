44e2938146c459ae4d42d36daa394652bd51cb8a
Tige Phillips <tigelane@mac.com>
2018-07-06 14:14:13 -0700

    change to the file and where I'm storing download and execution info

--- debian_install_docker.sh
+++ debian_install_docker.sh
@@ -1,5 +1,3 @@
-# wget -O debian_install_docker.sh https://raw.githubusercontent.com/tigelane/system_utilities/master/install%20_docker_ubuntu.sh
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
@@ -11,3 +9,8 @@ apt install docker-ce=17.03.2~ce-0~ubuntu-xenial
 #sudo apt-get install -y docker-ce
 #sudo systemctl status docker
 sudo usermod -aG docker ${USER}
+
+# Download and run
+exit
+wget -O debian_install_docker.sh https://raw.githubusercontent.com/tigelane/system_utilities/master/install%20_docker_ubuntu.sh
+chmod +x *_install_docker.#!/bin/sh
