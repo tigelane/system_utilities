70783c857c7e7a16f6c1a156e27e4f7717913e46
Tige Phillips <tigelane@mac.com>
2018-07-07 10:52:18 -0700

    test

--- ubuntu_install_docker.sh
+++ ubuntu_install_docker.sh
@@ -3,15 +3,14 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-apt-cache policy docker-ce
+sudo apt-cache policy docker-ce
 sudo apt-get -f install -y
-# folowing for debian
-apt install docker-ce=17.03.2~ce-0~ubuntu-xenial
-# Following for pure ubuntu
-#sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-ce
 sudo systemctl status docker
 sudo usermod -aG docker ${USER}
 
 # Download info
 exit
-wget -O ubuntu_install_docker.sh https://raw.githubusercontent.com/tigelane/system_utilities/master/ubuntu_install_docker.sh && chmod +x ubuntu_install_docker.sh
+wget -O ubuntu_install_docker.sh https://raw.githubusercontent.com/tigelane/system_utilities/master/ubuntu_install_docker.sh \
+    && chmod +x ubuntu_install_docker.sh \
+    && ./ubuntu_install_docker.sh
