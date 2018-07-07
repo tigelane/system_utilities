c69b6ce301dd7e96d21d166258924132b1d3f0c0
Tige Phillips <tigelane@mac.com>
2018-07-07 10:46:28 -0700

    Updates and additions

--- ubuntu_install_jenkins.sh
+++ ubuntu_install_jenkins.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
@@ -9,9 +8,9 @@ sudo apt-get -f install -y
 apt install docker-ce=17.03.2~ce-0~ubuntu-xenial
 # Following for pure ubuntu
 #sudo apt-get install -y docker-ce
-sudo systemctl status docker
+#sudo systemctl status docker
 sudo usermod -aG docker ${USER}
 
-# Download info
+# Download command for this script
 exit
-wget -O ubuntu_install_docker.sh https://raw.githubusercontent.com/tigelane/system_utilities/master/ubuntu_install_docker.sh %% chmod +x ubuntu_install_docker.sh
+wget -O debian_install_docker.sh https://raw.githubusercontent.com/tigelane/system_utilities/master/debian_install_docker.sh && chmod +x debian_install_docker.sh
