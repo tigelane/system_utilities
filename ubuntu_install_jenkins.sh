22dbfb65c49ef41a9c364f88d19379a595e3a753
Tige Phillips <tigelane@mac.com>
2018-07-09 13:05:09 -0700

    Change from debian to ubuntu

--- ubuntu_install_jenkins.sh
+++ ubuntu_install_jenkins.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-apt-cache policy docker-ce
-sudo apt-get -f install -y
-# folowing for debian
-apt install docker-ce=17.03.2~ce-0~ubuntu-xenial
-# Following for pure ubuntu
-#sudo apt-get install -y docker-ce
-#sudo systemctl status docker
-sudo usermod -aG docker ${USER}
+sudo apt update && sudo apt dist-upgrade && sudo apt autoremove
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c "echo 'deb https://pkg.jenkins.io/debian-stable binary/' > /etc/apt/sources.list.d/Jenkins.list"
+sudo apt update
+sudo apt install jenkins
+
 
 # Download command for this script
 exit
+sudo systemctl stop jenkins.service
+sudo systemctl start jenkins.service
+sudo systemctl enable jenkins.service
+
+http://localhost:8080
 wget -O debian_install_docker.sh https://raw.githubusercontent.com/tigelane/system_utilities/master/debian_install_docker.sh && chmod +x debian_install_docker.sh
