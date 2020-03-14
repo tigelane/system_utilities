a852573c49e6f5096942961c06294a68e7026ec2
tigelane <tige@ignw.io>
2020-03-14 16:11:03 -0700

    added packer for use in the netops lab

--- ubuntu18_install_github_actions_part1.sh
+++ ubuntu18_install_github_actions_part1.sh
@@ -5,6 +5,7 @@
 # Update system and install git
 sudo apt-get update
 sudo apt-get -y install git
+sudo apt-get -y install packer
 
 # Install Docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
