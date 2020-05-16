7f77815ca42809b2c95989f40537fe01da1d552f
tigelane <tige@ignw.io>
2020-05-16 09:08:39 -0700

    add installer for gitlab

---
+++ ubuntu18_install_gitlab_runner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Installs Gitlab Runner on Ubuntu 18.04 on GCP
+# --image=ubuntu-1804-bionic-v20200430
+
+sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+sudo chmod +x /usr/local/bin/gitlab-runner
+sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+sudo gitlab-runner start
+
+sudo gitlab-runner register
